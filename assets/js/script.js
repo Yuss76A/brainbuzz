a72e27d05d5c9ba560863285fee196f19cd1ef1c
Yusein Ali <yuse_91yus@hotmail.com>
2024-08-28 21:30:30 +0000

    Add variables. currenetQuestion,acceptinAnswers,score,questionCounter and availableQuestions to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
 const question = document.getElementById("question");
 const responses = Array.from(document.getElementsByClassName("response-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
