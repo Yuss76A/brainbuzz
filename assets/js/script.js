ac1a36d29d4a35ace80867f4efbb9f917df47ee0
Yusein Ali <yuse_91yus@hotmail.com>
2024-08-28 21:53:28 +0000

    Add constants bonus points and total questions to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,46 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+
+let questions = [{
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        option5: '',
+        answer: 1,
+    },
+    {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        option5: '',
+        answer: 1,
+    },
+    {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        option5: '',
+        answer: 1,
+    },
+    {
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        option5: '',
+        answer: 1,
+    },
+]
+
+// Fixed Values
+
+const BONUS_POINTS = 15;
+const TOTAL_QUESTIONS = 10;
