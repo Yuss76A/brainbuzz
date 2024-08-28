dc5ed84f095230fbe844755ad03d407d1874fc1e
Yusein Ali <yuse_91yus@hotmail.com>
2024-08-28 23:49:52 +0000

    Add padding propertie to the style.css file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,16 @@ let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let remainingQuestions = [];
 
 let questions = [{
-        question: '',
-        option1: '',
-        option2: '',
-        option3: '',
-        option4: '',
-        option5: '',
-        answer: 1,
+        question: '"What is the capital of France?',
+        option1: 'Berlin',
+        option2: 'Madrid',
+        option3: 'Paris',
+        option4: 'Rome',
+        option5: 'Lisbon',
+        answer: 3,
     },
     {
         question: '',
@@ -43,45 +43,49 @@ let questions = [{
         option5: '',
         answer: 1,
     },
-]
+];
 
 // Fixed Values
 
 const BONUS_POINTS = 15;
 const TOTAL_QUESTIONS = 10;
 
-initGame = () => {
-    questionCount = 0;
-    currentScore = 0;
-    remainingQuestion;
-    s = [...questions];
-    loadNewQuestion()
+const initGame = () => {
+    questionCounter = 0;
+    score = 0;
+    remainingQuestions = [...questions];
+    loadNewQuestion();
 };
 
-loadNewQuestion = () => {
-    if (remainingQuestions.length === 0 || questionCount >= TOTAL_QUESTIONS)
-        return window.location, assign('/end.html');
-}
-questionCount++;
-const randomIndex = Math.floor(Math.random() * remainingQuestions.length);
-currentQuestion = remainingQuestions[randomIndex];
-questionElement.innerText = currentQuestion.question;
+const loadNewQuestion = () => {
+    if (remainingQuestions.length === 0 || questionCounter >= TOTAL_QUESTIONS) {
+        return window.location.assign('/end.html');
+    }
 
-answerResponses.forEach((response) => {
-    const number = choice.dataset['number'];
-    response.innerText = currentQuestion['response' + number];
-});
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * remainingQuestions.length);
+    currentQuestion = remainingQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-remainingQuestions.splice(randomIndex, 1);
-isAcceptingAnswers = true;
+    responses.forEach((response) => {
+        const number = response.dataset['number'];
+        response.innerText = currentQuestion['option' + number];
+    });
 
+    remainingQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
 
-answerResponses.forEach((reponse) => {
+responses.forEach((response) => {
     response.addEventListener('click', (event) => {
-        if (!isAcceptingAnswers) return;
+        if (!acceptingAnswers) return;
 
-        isAcceptingAnswers = false;
+        acceptingAnswers = false;
         const chosenOption = event.target;
-        const chosenAnswer = chosenOption.dataset['number'];
+        const chosenAnswers = chosenOption.dataset['number'];
+
+        loadNewQuestion()
     });
 });
+
+initGame();
