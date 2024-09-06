5fd5f551b526cafb335dc5ee08972085bcee7a47
Yusein Ali <yuse_91yus@hotmail.com>
2024-09-06 22:24:24 +0000

    Add doc strings to the final.js file and remove spaces between comments and codes in the same file

--- assets/js/final.js
+++ assets/js/final.js
@@ -1,5 +1,12 @@
 // Final Page
+
+// Retrieve high scores from localStorage, or initialize with an empty array if none are available
 const scores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+
+/**
+ * Event listener for DOMContentLoaded to initialize form handling and display score.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const userInput = document.getElementById('user');
     const saveButton = document.getElementById('submitscorebtn');
@@ -7,46 +14,48 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('SCOREOUTPUT: ', scoreOutput)
 
     // Get recent score from localStorage
-
     const recentScore = localStorage.getItem('mostRecentScore');
     console.log('RECENTSCORE: ', recentScore)
     scoreOutput.innerText = recentScore || 0;
 
-    // Enable or disable the save button based on user input
 
+    /**
+     * Enables or disables the save button based on presence of user input.
+     */
     userInput.addEventListener('input', () => {
         saveButton.disabled = userInput.value.trim() === '';
     });
 
-    // Function to save the score
 
+    /**
+     * Saves the current score to local storage and redirects to the home page.
+     *
+     * @param {Event} event - The form submission event.
+     */
     const saveScore = (event) => {
         event.preventDefault();
         const username = userInput.value;
         const scoreToSave = recentScore;
 
+        // Create score object
         const submittedScore = {
             score: scoreToSave,
             username
         };
 
+        // Add and sort scores, keeping the top 5
         scores.push(submittedScore);
         scores.sort((a, b) => b.score - a.score);
         scores.splice(5);
         localStorage.setItem('highScores', JSON.stringify(scores));
 
-
-
         // Redirect to home page
-
-
-        window.location.href = '/index.html';
+        window.location.href = 'index.html';
 
     };
 
     scoreForm.addEventListener('submit', saveScore);
 
     // Listen for form submission
-
     saveButton.addEventListener('click', saveScore);
 });
